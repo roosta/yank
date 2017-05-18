d27eaef7e51c4bd822171b50f1ec1bbd1ea3ea35
Daniel Berg <mail@roosta.sh>
2017-05-18 21:50:01 +0200

    Add cljs repl support for background

--- resources/public/background.js
+++ resources/public/background.js
@@ -1,3 +1,3 @@
 var CLOSURE_UNCOMPILED_DEFINES = null;
-// goog.require("figwheel.connect.build_popup");
+goog.require("figwheel.connect.build_background");
 goog.require("save_to_org.background");
--- script/repl.clj
+++ script/repl.clj
@@ -1,3 +1,3 @@
 (use 'figwheel-sidecar.repl-api)
 (start-figwheel! "popup" "background") ;; <-- fetches configuration
-;; (cljs-repl)
+(cljs-repl)
