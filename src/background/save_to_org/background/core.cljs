334bfef35ebfb7fd9ff28bc17e49f9c4e300a879
Daniel Berg <mail@roosta.sh>
2017-05-27 20:24:29 +0200

    Get title, url and org-link format

--- src/background/save_to_org/background/core.cljs
+++ src/background/save_to_org/background/core.cljs
@@ -1,34 +1,22 @@
 (ns save-to-org.background.core
   (:require-macros [utils.logging :refer [log info]])
   (:require [goog.object :as gobj]
-            [goog.events :as events]
-            [oops.core :refer [oget oset! ocall oapply ocall! oapply!
-                               oget+ oset!+ ocall+ oapply+ ocall!+ oapply!+]]))
+            [goog.events :as events]))
 
 (defn update-tab
   [tabs]
   (when-let [current-tab (get tabs 0)]
-    (log (oget current-tab :url))))
+    (let [url (gobj/get current-tab "url")
+          title (gobj/get current-tab "title")]
+      #_(log (format "[[%1][%2]]" url title))
+      (log (str "[[" url "][" title "]]")))))
 
 (defn update-active-tab
   [tabs]
   (let [getting-active-tab (.query js/browser.tabs #js {:active true :currentWindow true})]
-    (.then getting-active-tab update-tab)
-    ))
+    (.then getting-active-tab update-tab)))
 
 (defn init!
   []
   (log "background init!")
-  (events/listen js/document (oget events/EventType :WHEEL) #(log %))
-  #_(log events/EventType)
-
-  ;; listen to tab URL changes
-  #_(.addListener js/browser.tabs.onUpdated update-active-tab)
-
-  ;; listen to tab switching
-  #_(.addListener js/browser.tabs.onActivated update-active-tab)
-
-  ;; listen for window switching
-  #_(.addListener js/browser.windows.onFocusChanged update-active-tab)
-
-  )
+  (.addListener js/browser.commands.onCommand update-active-tab))
