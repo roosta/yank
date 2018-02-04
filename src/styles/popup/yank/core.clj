9b7c930736e40ca13ad3cef6c0e3ed64e28aae6d
Daniel Berg <mail@roosta.sh>
2018-02-04 15:31:01 +0100

    Update html, use OS styles

--- src/styles/popup/yank/core.clj
+++ src/styles/popup/yank/core.clj
@@ -2,9 +2,10 @@
   (:require [garden.def :refer [defstylesheet defstyles]]
             [garden.units :refer [px]]))
 
-;; Change defstylesheet to defstyles.
 (defstyles screen
   [:body
-   {:font-family "sans-serif"
+   {:font "caption"
+    :color "WindowText"
+    :text-align "center"
     :font-size (px 16)
     :line-height 1.5}])
