463e4ce13398044c4e58a98a9ed83eb10863dc75
Daniel Berg <mail@roosta.sh>
2018-07-15 16:19:09 +0200

    Add markdown test

--- test/integration/yank/core_test.clj
+++ test/integration/yank/core_test.clj
@@ -38,7 +38,7 @@
   fixture-driver)
 
 (deftest ^:integration
-  org-mode-basic
+  org-mode
   (doto *driver*
     (go "https://google.com")
     (e/fill {:tag :body} k/escape)
@@ -47,4 +47,26 @@
                            (.getSystemClipboard)
                            (.getData (DataFlavor/stringFlavor)))
         expected "[[https://www.google.com/][Google]]"]
+    (is (= clipboard expected))))
+
+(deftest ^:integration
+  markdown
+  (doto *driver*
+    (e/go "about:addons")
+    (e/click :category-extension)
+    (e/click {:class "addon addon-view"})
+    (e/click{:class "addon-control preferences"}))
+  (let [window-handles (e/get-window-handles *driver*)]
+    (doto *driver*
+      (e/switch-window (last window-handles))
+      (e/click :format-select)
+      (e/fill :format-select k/arrow-down)
+      (e/click {:type :submit})
+      (e/go "https://google.com")
+      (e/fill {:tag :body} k/escape)
+      (e/fill {:tag :body} k/control-left "y" )))
+  (let [clipboard (-> (Toolkit/getDefaultToolkit)
+                      (.getSystemClipboard)
+                      (.getData (DataFlavor/stringFlavor)))
+        expected "[Google](https://www.google.com/)"]
     (is (= clipboard expected))))
