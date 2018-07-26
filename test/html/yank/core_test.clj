a4a0593555ba7b3e5edf83e186d06c4ac902e4cd
Daniel Berg <mail@roosta.sh>
2018-07-26 21:05:15 +0200

    Simplify background script test

--- test/html/yank/core_test.clj
+++ test/html/yank/core_test.clj
@@ -6,7 +6,7 @@
 
 (deftest background-html
 
-  (testing "Testing background-html using dev profile"
+  (testing "background-html using dev profile"
     (let [tree (-> (core/background-html true)
                    parse
                    as-hickory)
@@ -19,11 +19,7 @@
                       :charset)]
       (are [result expected] (= result expected)
         charset "utf-8"
-        (count scripts) 4
-        (-> (nth scripts 0) :attrs :src) "js/background/goog/base.js"
-        (-> (nth scripts 1) :attrs :src) "setup.js"
-        (-> (nth scripts 2) :attrs :src) "js/background/cljs_deps.js"
-        (-> (nth scripts 3) :attrs :src) "background.js")))
+        (map (comp :src :attrs) scripts) '("js/background/goog/base.js" "setup.js" "js/background/cljs_deps.js" "background.js"))))
 
   (testing "Testing background-html using release profile"
     (let [tree (-> (core/background-html false)
