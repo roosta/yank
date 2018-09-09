20a6dc012d94922e6a5b05e97f9377edc73906ae
Daniel Berg <mail@roosta.sh>
2018-09-09 15:28:12 +0200

    Include all the builds in start

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,7 @@
 (ns user
-  (:use [figwheel-sidecar.repl-api :as ra]))
+  (:require [figwheel-sidecar.repl-api :as ra]))
 
-(defn start [] (ra/start-figwheel!))
+(defn start [] (ra/start-figwheel! "background" "options" "popup"))
 
 (defn stop [] (ra/stop-figwheel!))
 
