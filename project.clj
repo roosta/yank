1cf772e33eda9c1dfadd59965bd675a96eee23de
Daniel Berg <mail@roosta.sh>
2018-09-11 18:13:45 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yank "1.0.3"
+(defproject yank "1.0.4"
   :description "Yank current page URL to clipboard as various markup formats"
   :url "https://github.com/roosta/yank"
   :license {:name "MIT"}
