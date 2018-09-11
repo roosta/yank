9c5f12cb9c46ae4d8c110cb9cb08680f289767ae
Daniel Berg <mail@roosta.sh>
2018-09-11 17:24:23 +0200

    Wait for elements in tests

--- test/integration/yank/core_test.clj
+++ test/integration/yank/core_test.clj
@@ -40,6 +40,7 @@
   basic
   (doto *driver*
     (e/go "https://google.com")
+    (e/wait-visible {:id "lst-ib"})
     (e/fill {:tag :body} k/escape)
     (e/fill {:tag :body} k/control-left "y" ))
   (let [clipboard (-> (Toolkit/getDefaultToolkit)
@@ -52,18 +53,21 @@
   change-opts
   (doto *driver*
     (e/go "about:addons")
+    (e/wait-visible :category-extension)
     (e/click :category-extension)
     (e/click {:class "addon addon-view"})
     (e/click {:class "addon-control preferences"}))
   (let [window-handles (e/get-window-handles *driver*)]
     (doto *driver*
       (e/switch-window (last window-handles))
+      (e/wait-visible :keybind-input)
       (e/click :keybind-input)
       (e/fill :keybind-input k/shift-left "l")
       (e/click :format-select)
       (e/fill :format-select k/arrow-down)
       (e/click {:type :submit})
       (e/go "https://google.com")
+      (e/wait-visible {:id "lst-ib"})
       (e/fill {:tag :body} k/escape)
       (e/fill {:tag :body} k/shift-left "l" )))
   (let [clipboard (-> (Toolkit/getDefaultToolkit)
