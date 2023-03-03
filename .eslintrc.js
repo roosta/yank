605aae620545112fe33283f8583c793e5510b7d1
Daniel Berg <mail@roosta.sh>
2023-03-03 16:19:31 +0100

    Configure eslint for extension env

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
       "browser": true,
       "es2021": true,
       "node": true,
+      "browser-window": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
