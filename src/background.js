9417c4e7cd9572c10cc3b400e6742b708fdd94da
Daniel Berg <mail@roosta.sh>
2023-03-10 15:11:46 +0100

    wip: escape and formatting

--- src/background.js
+++ src/background.js
@@ -14,11 +14,23 @@ function onError(error) {
   console.error(`Error: ${error}`);
 }
 
+// Escape string s, with array of tuples [regex, replacement text] i.e
+// stresc(mystr, [["_", "\\_"], ["[", "\\["]])
+function stresc(s, m) {
+  return m.reduce((acc, [re, repl]) => {
+    return acc.replace(re, repl)
+  }, s)
+
+}
+
 browser.commands.onCommand.addListener(async (command) => {
   if (command === "yank") {
     browser.tabs.query({currentWindow: true, active: true}, ([tab]) => {
-      const text = "tessssst"
-      browser.tabs.sendMessage(tab.id, { yank: text });
+      // const re = /_/g;
+      const text = "my fancy title _ with chars [hello]";
+      // const asd = text.replace(re, "\\_") ;
+      console.log(stresc(text, [["_", "\\_"], ["[", "\\["]]));
+      browser.tabs.sendMessage(tab.id, { yank: "asd" });
     }).then((response) => {
       if (!response.response)
         console.warn("Yank: bad response, something went wrong");
