06984ef4d914836069c8450dba5245ca1a1cf4ca
Daniel Berg <mail@roosta.sh>
2023-03-24 00:12:31 +0100

    Add production scripts, build files

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  watch: true,
   entry: {
     background: "./src/background.js",
-    popup: "./src/popup.js"
+    popup: "./src/popup.js",
   },
   output: {
     path: path.resolve(__dirname, "addon"),
-    filename: "[name]/index.js"
+    filename: "[name]/index.js",
   },
-  mode: 'none',
 };
